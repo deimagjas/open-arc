65cc11db7c4421b52038daa8af281afb53f16c29
Carlos Alberto  Argüello Ramírez <deimagjas@MacBook-Air-de-Carlos.local>
2025-04-15 21:05:53 -0500

    add: base project

---
+++ applications/app-service/src/main/java/cloud/qubits/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package cloud.qubits.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "cloud.qubits.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
